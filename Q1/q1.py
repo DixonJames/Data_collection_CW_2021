ab0f676aef4451585e120279b791717d1b736264
james <jamesmedixon@gmail.com>
2021-02-24 22:39:34 +0000

    simple distance calcs
    jaccard distance cal
    using glove dataset to vec the strings for cos distance calc

--- Q1/q1.py
+++ Q1/q1.py
@@ -1,133 +1,509 @@
+from bs4 import BeautifulSoup as soup
+from nltk.corpus import stopwords
+
 import pandas as pd
 import random
 import urllib3
-from bs4 import BeautifulSoup as soup
+import numpy as np
+import math
+
 import re
+import nltk
+import csv
+
+import numpy
+import scipy
+import sklearn
+
+try:
+    print("downloading common wordlists")
+    nltk.download('stopwords')
+    nltk.download('punkt')
+except:
+    print("couldn't download latest lists")
 
 path = "distance.xlsx"
+
+def tokenizeString(article):
+    """
+    removed common words and splits atricle into tokens in an array
+    :param article: one string
+    :return: list of words/tokens
+    """
+    import string
+    blacklist = set({"'",'``', '\'\'' })
+    s = set(stopwords.words('english'))
+    article = article.replace(".", "")
+    important_words = ""
+    for word in article.split():
+        if word not in s:
+            important_words += (re.sub("\\\\", "", word) + " ")
+
+    return [word.lower() for word in nltk.word_tokenize(important_words) if len(word) != 1 and (word not in blacklist) and word not in string.punctuation]
+
 
 
 class FileErr(Exception):
     pass
 
-
-def openXLS(path):
-    try:
-        keywords =  pd.read_excel(path)
-        return keywords
-    except Exception as e:
-        raise FileErr() from e
-
-def getPage(url, eaxtraHeaders):
-
-    usrAgnt = f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/{random.randint(0, 1000)}.{random.randint(0, 50)} (KHTML, like Gecko) Chrome/{random.randint(10, 99)}.{random.randint(10, 99)}.{random.randint(1000, 9999)}.{random.randint(100, 999)} Safari/{random.randint(100, 999)}.{random.randint(10, 99)}"
-
-    if eaxtraHeaders == None:
-        headers = { 'User-Agent' : usrAgnt }
-    else:
-        headers = ({ 'User-Agent' : usrAgnt }).update(eaxtraHeaders)
-
-    http = urllib3.PoolManager()
-    response = http.request('GET', url, headers = headers)
-    data = soup(response.data, "html.parser")
-
-    #print(extractBBCLinks(data))
-    return data
-
-def saveTextFile(name, contents, subfolderName = None):
-    import os.path
-
-    PROJECT_ROOT_DIR = "."
-    CHAPTER_ID = "BBC_pages"
-
-    if subfolderName == None:
-        save_path = os.path.join(PROJECT_ROOT_DIR, "data", CHAPTER_ID)
-    else:
-        save_path = os.path.join(PROJECT_ROOT_DIR, "data", CHAPTER_ID, subfolderName)
-
-    os.makedirs(save_path, exist_ok=True)
+class BBCextraction:
 
 
-    completeName = os.path.join(save_path, name + ".html")
+    def getPage(self, url, eaxtraHeaders):
 
-    with open(completeName, "w", encoding='utf-8') as file:
+        usrAgnt = f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/{random.randint(0, 1000)}.{random.randint(0, 50)} (KHTML, like Gecko) Chrome/{random.randint(10, 99)}.{random.randint(10, 99)}.{random.randint(1000, 9999)}.{random.randint(100, 999)} Safari/{random.randint(100, 999)}.{random.randint(10, 99)}"
+
+        if eaxtraHeaders == None:
+            headers = { 'User-Agent' : usrAgnt }
+        else:
+            headers = ({ 'User-Agent' : usrAgnt }).update(eaxtraHeaders)
+
+        http = urllib3.PoolManager()
+        response = http.request('GET', url, headers = headers)
+        data = soup(response.data, "html.parser")
+
+        #print(self.extractBBCLinks(data))
+        return data
+
+    def extractBBCLinks(self, data):
+        links = []
+        containers = data.findAll("div", {"class": "ssrcss-v4rel9-PromoContent e1f5wbog0"})
+        for c in containers:
+            url_section = c.findAll("div", {"class": "ssrcss-1uw1j0b-PromoHeadline e1f5wbog2"})
+            if c.a["href"].split("/")[3] == "news":
+                links.append(c.a["href"])
+
+        return list(set(links))
+
+    def searchPages(self):
+        """
+        goes though the XLM file and searches each item on bbc news and saves the resulting page in a text file
+        :return:
+        """
+        keywords = fileMangagement().openXLS(path)['Keywords']
+        testURL = "https://www.bbc.co.uk/search?q=durham"
+
+        for word in list(keywords):
+            seach_header = {'q': f"{word}"}
+            content = self.getPage(testURL, seach_header)
+            fileMangagement().saveTextFile(word, str(content), "base_searches")
+
+    def BBCsearch(self, word):
+        """
+        produces all links to articles from a certain search on BBC
+        :param word:
+        :return:
+        """
+        links = []
+        for p in range(30):
+            url = f"https://www.bbc.co.uk/search?q={word}&page={p}"
+            links.extend(self.extractBBCLinks(self.getPage(url, None)))
+
+        return links
+
+    def extractArticle(self, data):
+
+        links = []
         try:
-            file.write(contents)
+            title = str(data.findAll("h1", {"class": "ssrcss-1pl2zfy-StyledHeading e1fj1fc10"})[0].contents[0])
+        except:
+            title = str(data.findAll("h1")[0].contents[0])
+
+        paragraphs = data.findAll("p")
+        clean_paragraphs = [title]
+
+        url_section = []
+
+        for c in paragraphs:
+            url_section = c.findAll("a", {"class": "ssrcss-hiczm3-InlineLink e1no5rhv0"})
+
+            if url_section != []:
+                if c.a["href"]:
+                    links.append(c.a["href"])
+
+            if len(c.findAll("a", {"class": "ssrcss-1hlxxic-PromoLink e1f5wbog6"})) == 0 and len(c.contents) != 0:
+                if not ("class=" in str(c.contents[0])):
+                    clean_paragraphs.append( re.sub("([\(\[]).*?([\)\]])", "\g<1>\g<2>", (re.sub("\\\\", "", str(c.contents[0]))).replace("\\", ""))
+)
+
+            article = ""
+
+            for p in clean_paragraphs[:-1]:
+                article += p
+
+        return list(set(links)), article
+
+class fileMangagement:
+
+    def openXLS(self, path):
+        try:
+            keywords = pd.read_excel(path)
+            return keywords
+        except Exception as e:
+            raise FileErr() from e
+
+    def saveTextFile(self, name, contents, subfolderName=None, basefolder=None):
+        import os.path
+
+        PROJECT_ROOT_DIR = "."
+
+        if basefolder == None:
+            CHAPTER_ID = "BBC_pages"
+        else:
+            CHAPTER_ID = basefolder
+
+        if subfolderName == None:
+            save_path = os.path.join(PROJECT_ROOT_DIR, "data", CHAPTER_ID)
+        else:
+            save_path = os.path.join(PROJECT_ROOT_DIR, "data", CHAPTER_ID, subfolderName)
+
+        os.makedirs(save_path, exist_ok=True)
+
+        completeName = os.path.join(save_path, name + ".txt")
+
+        with open(completeName, "w", encoding='utf-8') as file:
+            try:
+                file.write(contents)
+            except:
+                print("file write issue")
+
+
+    def getContent(self, keywords):
+        """
+        goes though all collected data and returns 2 dcitarys containing the article and inportat words for
+        each keyword
+
+        """
+        import os
+        PROJECT_ROOT_DIR = "."
+
+
+        #for each keyword makes one giant article
+        articles_path = os.path.join(PROJECT_ROOT_DIR, "data", "Articles")
+
+        directory = os.fsencode(articles_path)
+
+        totalAritcles = {}
+        for keyword in keywords:
+            allArticels = ""
+
+            for folder in os.listdir(directory):
+                foldername = os.fsdecode(folder)
+
+                if keyword in foldername:
+                    for file_name in os.listdir(os.path.join(PROJECT_ROOT_DIR, "data", "Articles", foldername)):
+                        if file_name.endswith(".txt"):
+                            filename_str = os.fsdecode(file_name)
+                            with open(os.path.join(PROJECT_ROOT_DIR, "data", "Articles", foldername, filename_str), "r", encoding="utf8") as f:
+                                allArticels += re.sub("([\<(\[]).*?([\)\]\>])", "",(f.read()))
+
+            totalAritcles[f"{keyword}"] = allArticels
+
+
+        words_path = os.path.join(PROJECT_ROOT_DIR, "data", "Prominent_words")
+
+        totalDataFrames = {}
+        for keyword in keywords:
+            allArticels = ""
+
+            for folder in os.listdir(words_path):
+                foldername = os.fsdecode(folder)
+
+                if keyword in foldername:
+                    for file_name in os.listdir(os.path.join(PROJECT_ROOT_DIR, "data", "Prominent_words", foldername)):
+                        if file_name.endswith(".csv"):
+                            filename_str = os.fsdecode(file_name)
+
+                            try:
+                                totalDataFrames[f"{keyword}"] = pd.read_csv(os.path.join(PROJECT_ROOT_DIR, "data", "Prominent_words", foldername, filename_str))
+                            except:
+                                print("file open issue")
+
+
+
+        return totalAritcles, totalDataFrames
+
+
+
+    def saveCSVFile(self, name, contents, subfolderName = None, baseFolder = None):
+        import os.path
+
+        PROJECT_ROOT_DIR = "."
+
+        if baseFolder == None:
+            CHAPTER_ID = "BBC_pages"
+        else:
+            CHAPTER_ID = baseFolder
+
+
+        if subfolderName == None:
+            save_path = os.path.join(PROJECT_ROOT_DIR, "data", CHAPTER_ID)
+        else:
+            save_path = os.path.join(PROJECT_ROOT_DIR, "data", CHAPTER_ID, subfolderName)
+
+        os.makedirs(save_path, exist_ok=True)
+
+        try:
+            pd.DataFrame(contents).to_csv(os.path.join(save_path, f"{name}.csv",))
         except:
             print("file write issue")
 
 
-def extractBBCLinks(data):
-    links = []
-    containers = data.findAll("div", {"class": "ssrcss-v4rel9-PromoContent e1f5wbog0"})
-    for c in containers:
-        url_section = c.findAll("div", {"class": "ssrcss-1uw1j0b-PromoHeadline e1f5wbog2"})
-        if c.a["href"].split("/")[3] == "news":
-            links.append(c.a["href"])
+    def downloadFile(self, url, CHAPTER_ID = None):
+        import requests
+        import os.path
 
-    return list(set(links))
+        PROJECT_ROOT_DIR = "."
+
+        if CHAPTER_ID == None:
+            CHAPTER_ID = "pretrainedModels"
+
+        save_path = os.path.join("models", CHAPTER_ID)
+
+        os.makedirs(save_path, exist_ok=True)
+
+        r = requests.get(url)
+
+        # open method to open a file on your system and write the contents
+        with open("models/pretrainedModels/glove.6B.zip", "wb") as code:
+            code.write(r.content)
+
+class dataCollection:
+
+    def keywordDataCollection(self):
+        keywords = fileMangagement().openXLS(path)['Keywords']
+        testURL = "https://www.bbc.co.uk/search"
+
+        # testing
+        # keywords = ["Durham"]
+        wordnum = 0
+        for word in list(keywords):
+            wordnum += 1
+
+            links = []
+            prominent_words = []
+            first_word = 0
+            while len(links) < 100 and first_word <= len(word):
+                s_parts = word.split(" ")[first_word:]
+                searchword = ""
+                for part in s_parts:
+                    searchword += (part + " ")
+                searchword = searchword[:-1]
+
+                links.extend(BBCextraction().BBCsearch(searchword))
+                links = list(set(links))
+                first_word += 1
+
+            i = 0
+            for link in links:
+                print(int((i/len(links))*100),"%, of:",wordnum, "/", len(list(keywords)), "keywords")
+                i += 1
+
+                content = BBCextraction().getPage(link, None)
+                fileMangagement().saveTextFile(f"{word}_page_{i}", str(content), f"{word}_pages")
+
+                externalLinks, article = BBCextraction().extractArticle(content)
+                fileMangagement().saveTextFile(f"{word}_article_{i}", article, f"{word}_articles", "Articles")
+
+                prominent_words.extend(tokenizeString(article))
+
+            fileMangagement().saveCSVFile(f"{word}_prominent_words", list(set(prominent_words)), f"{word}", "Prominent_words")
+
+class similarity:
+    def __init__(self, keywords, articles):
+        self.dict_keywords = keywords
+        self.article = articles
+
+        self.vectorisingDict = {}
+
+    def getVecSources(self):
+        from pathlib import Path
+        import zipfile
+        url = "http://nlp.stanford.edu/data/glove.6B.zip"
+
+        # checks to see if has downloaded file
+
+        my_file = Path("models/pretrainedModels/glove.6B.zip")
+        if not(my_file.is_file()):
+            print("File not accessible")
+            print("Downloading resource file. This may take roughly 10 minutes depending on connection speeds!")
+            fileMangagement().downloadFile(url, "pretrainedModels")
+
+        #if not unzipped, unzips zip file
+        my_file = Path("glove.6B.50d.txt")
+        if not(my_file.is_file()):
+            print("extracting ZIP")
+
+            with zipfile.ZipFile("models/pretrainedModels/love.6B.zip", 'r') as zip_ref:
+                zip_ref.extractall(".")
+
+        my_file = Path("glove.6B.50d.txt")
+
+        if  not(my_file.is_file()):
+            print("files Acquired UNsuccessfully")
 
 
-def searchPages():
-    """
-    goes though the XLM file and searches each item on bbc news and saves the resulting page in a text file
-    :return:
-    """
-    keywords = openXLS(path)['Keywords']
-    testURL = "https://www.bbc.co.uk/search?q=durham"
-
-    for word in list(keywords):
-        seach_header = {'q': f"{word}"}
-        content  = getPage(testURL, seach_header)
-        saveTextFile(word, str(content), "base_searches")
 
 
 
-def BBCsearch(word):
-    """
-    produces all links to articles from a certain search on BBC
-    :param word:
-    :return:
-    """
-    links = []
-    for p in range(30):
-        url = f"https://www.bbc.co.uk/search?q={word}&page={p}"
-        links.extend(extractBBCLinks(getPage(url, None)))
 
-    return links
+    def vectoriseString(self, string):
+        #checks to see if correct resources have been downloaded, and gets them if nessisary
+        self.getVecSources()
+
+        if self.vectorisingDict == {}:
+            word_dict = {}
+            with open("glove.6B.50d.txt", 'r', encoding="utf-8") as f:
+                for line in f:
+                    word_dict[line.split()[0]] = np.array(line.split()[1:], "float32")
+
+            self.vectorisingDict = word_dict
+
+        if string in self.vectorisingDict.keys():
+            return self.vectorisingDict[string]
+        else:
+            return None
 
 
 
+        #now should have required files!
+
+    def jaccardSimilarity(self, setA_keywords, setB_keywords):
+        """
+        captain jaccard of the USS dataprise!
+        :return: ratio of common keywords in sets
+        """
+        common = (setA_keywords).intersection(setB_keywords)
+        union = (setA_keywords).union(setB_keywords)
+
+        return len(common) / len(union)
+
+    def avgVecKeywords(self, keywords, lengthVec):
+        a_vec = [0 for i in range(lengthVec)]
+        for w in list(keywords):
+            w_vec = self.vectoriseString(w)
+            if not(w_vec is None):
+                a_vec = np.add(a_vec, w_vec)
+
+        for i in range(len(a_vec)):
+            a_vec[i] = a_vec[i] / lengthVec
+
+        return a_vec
+
+    def cosineSimilarity(self, setA_keywords, setB_keywords):
+        from scipy import spatial
+        a_vec = self.avgVecKeywords(setA_keywords["0"], 50)
+        b_vec = self.avgVecKeywords(setB_keywords["0"], 50)
+
+        similarity = 1 - spatial.distance.cosine(a_vec, b_vec)
+
+        return similarity
+
+
+
+
+
+
+def jacSimDF(words_dict, article_dict, keywords):
+    keywordlist = list(keywords)
+    res = pd.DataFrame([[0.0 for i in range(len(keywordlist))] for i in range(len(keywordlist))], columns=keywordlist)
+
+    allsets = [set(words_dict[list(words_dict)[i]]["0"]) for i in range(len(list(keywords)))]
+    common = (allsets[0]).intersection(set(allsets[0]))
+    for s in allsets:
+        common = (common).intersection(s)
+
+    for keywordA in list(keywords):
+        for keywordB in list(keywords):
+            word_index = keywordlist.index(keywordB)
+
+            distance = similarity(words_dict, article_dict).jaccardSimilarity(set(words_dict[keywordA]["0"]) - common, set(words_dict[keywordB]["0"]) - common)
+            if distance == 1.0:
+                res[keywordA][word_index] = 0.0
+            else:
+                res[keywordA][word_index] = distance
+
+    return res
+
+def semanticSimDF(words_dict, article_dict, keywords):
+    keywordlist = list(keywords)
+    res = pd.DataFrame([[0.0 for i in range(len(keywordlist))] for i in range(len(keywordlist))], columns=keywordlist)
+    workspace = similarity(words_dict, article_dict)
+
+    for keywordA in list(keywords):
+        for keywordB in list(keywords):
+            word_index = keywordlist.index(keywordB)
+
+            distance = workspace.cosineSimilarity(words_dict[keywordA], words_dict[keywordB])
+
+            res[keywordA][word_index] = distance
+
+    return res
+
+
+def normaliseDF(df):
+    min, max = dfEdges(df)
+
+    for col_name in df.columns:
+        vals = df[f"{col_name}"]
+        new_vals = []
+        for val in vals:
+            new_vals.append((val - min)/(max - min))
+
+        df[f"{col_name}"] = new_vals
+
+
+
+
+    return df
+
+
+
+
+    return min, max
+
+
+def dfEdges(df):
+
+    min = 1.0
+    max = 0.0
+    for col_name in df.columns:
+        col_min = sorted(list(df[col_name]))[1]
+        col_max = sorted(list(df[col_name]))[-1]
+
+        if col_min < min:
+            min = col_min
+        if col_max > max:
+            max = col_max
+
+    return min, max
 
 if __name__ == '__main__':
-    keywords = openXLS(path)['Keywords']
-    testURL = "https://www.bbc.co.uk/search"
+    #gets all data:
+    #dataCollection().keywordDataCollection()
+    keywords = fileMangagement().openXLS(path)['Keywords']
+    article_dict, words_dict = fileMangagement().getContent(keywords)
+
+    comparisonEngine = similarity(words_dict, article_dict)
+
+    comparisonEngine.vectoriseString("hello")
 
 
-    #testing
-    #keywords = ["Durham"]
+    j_df = jacSimDF(words_dict, article_dict, keywords)
+    #dfEdges(df)
 
-    for word in list(keywords):
-        links = []
+    #s_df = semanticSimDF(words_dict, article_dict, keywords)
 
-        first_word = 0
-        while len(links) < 100 and first_word <= len(word):
-            s_parts = word.split(" ")[first_word:]
-            searchword = ""
-            for part in s_parts:
-                searchword += (part + " ")
-            searchword = searchword[:-1]
+    #j_df_n = normaliseDF(j_df)
+    #s_df = normaliseDF(s_df)
+
+    #now normailse the DS
+
+    print(j_df)
 
 
-            links.extend(BBCsearch(searchword))
-            links = list(set(links))
-            first_word += 1
 
-        i = 0
-        for link in links:
-            i += 1
-            content  = getPage(link, None)
 
-            saveTextFile(f"{word}_page_{i}", str(content), f"{word}_pages")
+
 
 
